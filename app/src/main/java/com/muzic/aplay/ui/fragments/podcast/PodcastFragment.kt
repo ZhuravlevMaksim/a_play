ebfaaec89e13ac0de3b50436e4ed499a0cc8b6b1
m.zhuravlev <maks723@yandex.ru>
2021-02-27 22:38:59 +0300

    update versions, add itunes podcast request

--- app/src/main/java/com/muzic/aplay/ui/fragments/podcast/PodcastFragment.kt
+++ app/src/main/java/com/muzic/aplay/ui/fragments/podcast/PodcastFragment.kt
@@ -1,5 +1,7 @@
 package com.muzic.aplay.ui.fragments.podcast
 
+import android.app.SearchManager
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,8 +9,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.muzic.aplay.R
 import com.muzic.aplay.ui.setTopAppBarTitle
+import timber.log.Timber
 
-class PodcastFragment: Fragment() {
+class PodcastFragment : Fragment() {
+
+    val TAG = javaClass.simpleName
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
         inflater.inflate(R.layout.podcast_fragment, container, false)
@@ -16,7 +21,45 @@ class PodcastFragment: Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         setTopAppBarTitle("A Podcast")
+
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo(itunesService)
+        itunesRepo.searchByTerm("Android Developer") {
+            Timber.i(TAG, "Results = $it")
+        }
     }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY) ?: return
+            performSearch(query)
+        }
+    }
+
+    private fun performSearch(term: String) {
+        val itunesService = ItunesService.instance
+        val itunesRepo = ItunesRepo(itunesService)
+        itunesRepo.searchByTerm(term) {
+            Timber.i(TAG, "Results = $it")
+        }
+    }
+
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//
+//            val inflater = menuInflater
+//            inflater.inflate(R.menu.menu_search, menu)
+//            val searchMenuItem = menu.findItem(R.id.search_item)
+//            val searchView = searchMenuItem?.actionView as SearchView
+//
+//            val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+//
+//            searchView.setSearchableInfo(
+//                searchManager.getSearchableInfo(componentName)
+//            )
+//            return true
+//        }
+//    }
 
 
 }
---
+++ app/src/main/java/com/muzic/aplay/ui/fragments/podcast/SearchViewModel.kt
@@ -0,0 +1,37 @@
+package com.muzic.aplay.ui.fragments.podcast
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class SearchViewModel(application: Application) :
+    AndroidViewModel(application) {
+    var iTunesRepo: ItunesRepo? = null
+
+    fun searchPodcasts(term: String, callback: (List<PodcastSummaryViewData>) -> Unit) {
+        iTunesRepo?.searchByTerm(term) { results ->
+            if (results == null) {
+                callback(emptyList())
+            } else {
+                val searchViews = results.map { podcast -> itunesPodcastToPodcastSummaryView(podcast) }
+                callback(searchViews)
+            }
+        }
+    }
+}
+
+data class PodcastSummaryViewData(
+    var name: String? = "",
+    var lastUpdated: String? = "",
+    var imageUrl: String? = "",
+    var feedUrl: String? = ""
+)
+
+private fun itunesPodcastToPodcastSummaryView(itunesPodcast: PodcastResponse.ItunesPodcast): PodcastSummaryViewData {
+    return PodcastSummaryViewData(
+        itunesPodcast.collectionCensoredName,
+        itunesPodcast.releaseDate,
+        itunesPodcast.artworkUrl30,
+        itunesPodcast.feedUrl
+    )
+}
+
